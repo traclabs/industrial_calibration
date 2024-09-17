dec72f8741ea77c0d91e65667f63f51a20f01193
Michael Ripperger <michael.ripperger@swri.org>
2024-09-17 08:50:28 -0500

    Duplicated extrinsic hand eye calibration widget and application

---
+++ gui/include/industrial_calibration/gui/extrinsic_hand_eye_calibration_2d_widget.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <industrial_calibration/target_finders/opencv/target_finder.h>
+
+#include <boost_plugin_loader/plugin_loader.h>
+#include <memory>
+#include <QMainWindow>
+#include <QDialog>
+
+class QTreeWidgetItem;
+
+namespace Ui
+{
+class ExtrinsicHandEyeCalibration2D;
+}
+
+namespace industrial_calibration
+{
+class ExtrinsicHandEyeResult;
+class TargetFinderWidget;
+class CameraIntrinsicsWidget;
+class TransformGuess;
+
+class ExtrinsicHandEyeCalibration2DWidget : public QMainWindow
+{
+public:
+  explicit ExtrinsicHandEyeCalibration2DWidget(QWidget* parent = nullptr);
+  ~ExtrinsicHandEyeCalibration2DWidget();
+
+  /**
+   * @brief Loads the calibration configuration from file
+   * @throws Exception on failure
+   */
+  void loadConfig(const std::string& config_file);
+
+  /**
+   * @brief Loads the calibration observations from file
+   * @throws Exception on failure
+   */
+  void loadObservations(const std::string& observations_file);
+
+  /**
+   * @brief Performs the calibration
+   * @throws Exception on failure
+   */
+  void calibrate();
+
+  /**
+   * @brief Saves results of the calibration
+   * @throws Exception on failure
+   */
+  void saveResults(const std::string& file);
+
+protected:
+  void closeEvent(QCloseEvent* event) override;
+
+  void onLoadConfig();
+  void onLoadObservations();
+  void onCalibrate();
+  void onSaveResults();
+
+  void loadTargetFinder();
+  void drawImage(QTreeWidgetItem* item, int col);
+
+  Ui::ExtrinsicHandEyeCalibration2D* ui_;
+  TargetFinderWidget* target_finder_widget_;
+  CameraIntrinsicsWidget* camera_intrinsics_widget_;
+  TransformGuess* camera_transform_guess_widget_;
+  TransformGuess* target_transform_guess_widget_;
+
+  boost_plugin_loader::PluginLoader loader_;
+  TargetFinderFactoryOpenCV::ConstPtr factory_;
+
+  std::shared_ptr<ExtrinsicHandEyeResult> result_;
+  TargetFinderOpenCV::ConstPtr target_finder_;
+};
+
+}  // namespace industrial_calibration
